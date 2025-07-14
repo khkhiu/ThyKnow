3881cefb489147132f6030624de257e1f2087c96
khkhiu <khkhiu@gmail.com>
2025-07-14 21:35:44 +0800

    Removed old html implementation

--- nodejs/public/miniapp/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-// nodejs/public/miniapp/src/types/index.ts
-// Core type definitions for the ThyKnow app
-
-import { PromptType } from './promptTypes';
-
-// Export the type
-export { PromptType };
